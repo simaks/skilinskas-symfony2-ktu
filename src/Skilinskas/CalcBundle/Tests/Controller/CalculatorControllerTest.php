f90e9ace25d80b8da93d2d698620fe4e952282f6
simas <simskil@gmail.com>
2014-11-18 22:32:57 +0200

    Advanced test for add

---
+++ src/Skilinskas/CalcBundle/Tests/Controller/CalculatorControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Skilinskas\CalcBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class CalculatorControllerTest extends WebTestCase
+{
+    public function getValidAddData()
+    {
+        return [
+            [1, 2, 3],
+            [1, 0, 1],
+            [100, -111.01, -11.01],
+        ];
+    }
+
+    /**
+     * @dataProvider getValidAddData
+     */
+    public function testAdd($a, $b, $c)
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/calc/add/'.$a.'/'.$b.'/');
+
+        $response=  json_decode($client->getResponse()->getContent());
+
+        $this->assertEquals(true, $response->success);
+        $this->assertEquals($c, $response->ans);
+    }
+}
